621c19a9b8e13d572db83d87fae5d4d70c3bff78
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2026-06-17 14:47:37 -0400

    Add rudimentary parsing.

--- src/main.rs
+++ src/main.rs
@@ -29,14 +29,23 @@ fn main() -> std::io::Result<()> {
         let p = Path::new(&path);
         let bytes = fs::read(p).unwrap();
 
-        if !bytes.starts_with(&[0x4d, 0x54, 0x68, 0x64]) {
+        if  !bytes.starts_with(&[0x4d, 0x54, 0x68, 0x64]) &&
+            !bytes.ends_with(&[0x00, 0x00, 0xFF, 0x2F, 0x00]) {
             return Err(std::io::Error::new(
                 ErrorKind::InvalidInput,
-                "This file is corrupted or the filetype is unsupported.",
+                "This file is corrupted, incomplete, and/or the filetype is unsupported.",
             ));
         }
 
-        
+        let len: u32 = u32::from_be_bytes([bytes[4], bytes[5], bytes[6], bytes[7]]);
+        let mode: u16 = u16::from_be_bytes([bytes[8], bytes[9]]);
+        let num_tracks: u16 = u16::from_be_bytes([bytes[10], bytes[11]]);
+        let tpq: u16 = u16::from_be_bytes([bytes[12], bytes[13]]);
+
+        // This at least works for the midi I'm working with, will extend my testing set in the near future.
+        let name_len = bytes[25] as usize;
+        let name = String::from_utf8(bytes[26..(26 + name_len)].to_vec()).unwrap();
+        println!("The name of the song is: {}", name);
     }
 
     Ok(())
