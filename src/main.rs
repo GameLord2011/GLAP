6ab5a3432754f69dab00678d866f38e2b75276ae
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2026-06-23 13:20:09 -0400

    Delightfulness in a git commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fs,
-    io::{ErrorKind, stdin},
+    io::{Error, ErrorKind, stdin},
     path::Path,
 };
 
@@ -9,12 +9,18 @@ struct HeaderChunk {
     length: u32,
     mode: u16,
     num_tracks: u16,
-    tpq: u16
+    tpq: u16,
 }
 
 struct Chunk {
     flag: [u8; 4],
     length: u32,
+    data: Vec<u8>,
+}
+
+struct Message {
+    delta: Vec<u8>, // Erm acktually the MIDI format uses Variable Length Quantities to represent the delta.
+    status: u8,
     data: Vec<u8>
 }
 
@@ -43,39 +49,72 @@ fn main() -> std::io::Result<()> {
 
     if path.is_empty() {
         println!("You can't point me to nothing, sorry!");
-    } else {
-        let p = Path::new(&path);
-        let bytes = fs::read(p).unwrap();
+        return Err(Error::new(
+            ErrorKind::InvalidFilename,
+            "You can't point me to nothing, sorry!",
+        ));
+    }
+    let p = Path::new(&path);
+    let bytes = fs::read(p).unwrap();
 
-        let header: HeaderChunk = HeaderChunk {
-            flag: [bytes[0], bytes[1], bytes[2], bytes[3]],
-            length: u32::from_be_bytes([bytes[4], bytes[5], bytes[6], bytes[7]]),
-            mode: u16::from_be_bytes([bytes[8], bytes[9]]),
-            num_tracks: u16::from_be_bytes([bytes[10], bytes[11]]),
-            tpq: u16::from_be_bytes([bytes[12], bytes[13]])
-        };
+    let header: HeaderChunk = HeaderChunk {
+        flag: [bytes[0], bytes[1], bytes[2], bytes[3]],
+        length: u32::from_be_bytes([bytes[4], bytes[5], bytes[6], bytes[7]]),
+        mode: u16::from_be_bytes([bytes[8], bytes[9]]),
+        num_tracks: u16::from_be_bytes([bytes[10], bytes[11]]),
+        tpq: u16::from_be_bytes([bytes[12], bytes[13]]),
+    };
 
-        if header.flag != [0x4D, 0x54, 0x68, 0x64]
-        {
-            return Err(std::io::Error::new(
-                ErrorKind::InvalidInput,
-                "The header flag is incorrect.",
-            ));
-        }
+    if header.flag != [0x4D, 0x54, 0x68, 0x64] {
+        return Err(Error::new(
+            ErrorKind::InvalidInput,
+            "The header flag is incorrect.",
+        ));
+    }
 
-        let mut tracks: Vec<Chunk> = vec![];
+    let mut tracks: Vec<Chunk> = vec![];
 
-        let mut next_chunk_offset: usize = (header.length + 8).try_into().unwrap();
-        for _ in 0..header.num_tracks {
-            println!("Offset: {}", next_chunk_offset);
-            let l = u32::from_be_bytes([bytes[next_chunk_offset + 4], bytes[next_chunk_offset + 5], bytes[next_chunk_offset + 6], bytes[next_chunk_offset + 7]]);
-            tracks.insert(tracks.len(), Chunk {
-                flag: [bytes[next_chunk_offset], bytes[next_chunk_offset + 1], bytes[next_chunk_offset + 2], bytes[next_chunk_offset + 3]],
+    let mut next_chunk_offset: usize = (header.length + 8).try_into().unwrap();
+    for _ in 0..header.num_tracks {
+        println!("Offset: {}", next_chunk_offset);
+        let l = u32::from_be_bytes([
+            bytes[next_chunk_offset + 4],
+            bytes[next_chunk_offset + 5],
+            bytes[next_chunk_offset + 6],
+            bytes[next_chunk_offset + 7],
+        ]);
+        tracks.insert(
+            tracks.len(),
+            Chunk {
+                flag: [
+                    bytes[next_chunk_offset],
+                    bytes[next_chunk_offset + 1],
+                    bytes[next_chunk_offset + 2],
+                    bytes[next_chunk_offset + 3],
+                ],
                 length: l,
-                data: bytes[(next_chunk_offset + 4)..(next_chunk_offset + 4 + l as usize)].to_vec()
-            });
-            next_chunk_offset += 8 + l as usize;
+                data: bytes[(next_chunk_offset + 4)..(next_chunk_offset + 4 + l as usize)].to_vec(),
+            },
+        );
+        next_chunk_offset += 8 + l as usize;
+    }
+
+    let mut valid_tracks = true;
+    let mut first_inv_chunk = 0;
+
+    for (i, j) in tracks.iter().enumerate() {
+        if (j.flag != [0x4D, 0x54, 0x72, 0x6B]) || j.data.ends_with(&[0x00, 0xFF, 0x2F, 0x00]) {
+            valid_tracks = false;
+            first_inv_chunk = i;
+            break;
         }
+    }
+
+    if valid_tracks == false {
+        return Err(Error::new(
+            ErrorKind::InvalidInput,
+            format!("Chunk {first_inv_chunk} has an invalid flag or no end command!")
+        ))
     }
 
     Ok(())
