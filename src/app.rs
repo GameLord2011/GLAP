aac55ef46f5232fe0aa71c0e83e3f01f5d52da37
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2026-07-25 15:07:11 -0400

    UI, and also about page

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,19 @@
 use std::io;
 
-use ratatui::{DefaultTerminal, Frame};
+use crossterm::event::{Event::Key, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers};
+use ratatui::{
+    DefaultTerminal, Frame,
+    style::Stylize,
+    symbols::border,
+    text::Line,
+    widgets::{Block, Paragraph, Wrap},
+};
 
 #[derive(Default)]
 enum Page {
     About,
-    #[default] Player
+    #[default]
+    Player,
 }
 
 #[derive(Default)]
@@ -16,22 +24,80 @@ pub struct App {
 
 impl App {
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
-        terminal.draw(|frame| self.draw(frame))?;
-        self.handle_events()?;
+        loop {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+            if self.exit {
+                break;
+            }
+        }
         Ok(())
     }
 
     fn draw(&self, frame: &mut Frame) {
+        let mut block = Block::bordered().border_set(border::DOUBLE);
         match self.page {
             Page::About => {
-                
-                frame.render_widget(widget, frame.area());
-            },
-            Page::Player => {}
+                block = block
+                    .title(Line::from(" GLAP | About ").left_aligned())
+                    .title_bottom(Line::from(vec![
+                        " Quit ".into(),
+                        "<CTRL + Q>".green().bold(),
+                        " Player ".into(),
+                        "<CTRL + P> ".green().bold(),
+                    ]));
+                let inner_area = block.inner(frame.area());
+                let about_para =
+                    Paragraph::new(include_str!("about.txt")).wrap(Wrap { trim: false });
+                frame.render_widget(block, frame.area());
+                // frame.render_widget(RatatuiLogo::small(), inner_area);
+                frame.render_widget(about_para, inner_area);
+            }
+            Page::Player => {
+                block = block
+                    .title(Line::from(" GLAP | Player ").left_aligned())
+                    .title_bottom(Line::from(vec![
+                        " Quit ".into(),
+                        "<CTRL + Q>".green().bold(),
+                        " About ".into(),
+                        "<CTRL + A> ".green().bold(),
+                    ]));
+                frame.render_widget(block, frame.area())
+            }
         }
     }
 
     fn handle_events(&mut self) -> io::Result<()> {
-        todo!()
+        match crossterm::event::read()? {
+            Key(KeyEvent {
+                code: KeyCode::Char('a'),
+                modifiers: KeyModifiers::CONTROL,
+                kind: KeyEventKind::Press,
+                state: KeyEventState::NONE,
+            }) => self.page = Page::About,
+            Key(KeyEvent {
+                code: KeyCode::Char('p'),
+                modifiers: KeyModifiers::CONTROL,
+                kind: KeyEventKind::Press,
+                state: KeyEventState::NONE,
+            }) => self.page = Page::Player,
+            Key(KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: KeyModifiers::CONTROL,
+                kind: KeyEventKind::Press,
+                state: KeyEventState::NONE,
+            }) => self.exit = true,
+            _ => (),
+        }
+        Ok(())
     }
 }
+
+// impl Widget for &App {
+//     fn render(self, area: Rect, buf: &mut Buffer) {
+//         let title = Line::from("GLAP".bold());
+//         let block = Block::bordered()
+//             .title(title.centered())
+//             .border_set(border::DOUBLE);
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate sdl2;
 
+mod app;
 mod audio_player;
 mod player;
-mod app;
 
 use std::{default::Default, fs::File, io::stdin, time::Duration};
 
@@ -37,93 +37,94 @@ enum Page {
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
-    let config_dir = dirs::config_dir().unwrap();
-    let music_folder = dirs::audio_dir().unwrap();
-    let sdl_context = sdl2::init().unwrap();
-    let audio_subsystem = sdl_context.audio().unwrap();
-    let mut path = String::new();
-    let p = std::env::args().nth(1);
-    match p {
-        Some(n) => path = n,
-        None => {
-            println!("Whar is the file:");
-            stdin().read_line(&mut path)?;
-        }
-    }
-    path = path.trim_matches(['\r', '\n', '"', '\'']).to_owned();
+    ratatui::run(|terminal| crate::app::App::default().run(terminal))?;
+    // let config_dir = dirs::config_dir().unwrap();
+    // let music_folder = dirs::audio_dir().unwrap();
+    // let sdl_context = sdl2::init().unwrap();
+    // let audio_subsystem = sdl_context.audio().unwrap();
+    // let mut path = String::new();
+    // let p = std::env::args().nth(1);
+    // match p {
+    //     Some(n) => path = n,
+    //     None => {
+    //         println!("Whar is the file:");
+    //         stdin().read_line(&mut path)?;
+    //     }
+    // }
+    // path = path.trim_matches(['\r', '\n', '"', '\'']).to_owned();
 
-    if path.is_empty() {
-        println!("You can't point me to nothing, sorry!");
-    }
+    // if path.is_empty() {
+    //     println!("You can't point me to nothing, sorry!");
+    // }
 
-    let mut v = String::new();
-    println!("What do you want the volume to be out of 100?");
-    stdin().read_line(&mut v)?;
-    let d = v.trim_end().parse::<f32>().unwrap_or(100_f32);
-    let volume = d / 100_f32;
+    // let mut v = String::new();
+    // println!("What do you want the volume to be out of 100?");
+    // stdin().read_line(&mut v)?;
+    // let d = v.trim_end().parse::<f32>().unwrap_or(100_f32);
+    // let volume = d / 100_f32;
 
-    let file = Box::new(File::open(path).unwrap());
-    let mss = MediaSourceStream::new(file, Default::default());
+    // let file = Box::new(File::open(path).unwrap());
+    // let mss = MediaSourceStream::new(file, Default::default());
 
-    let hint = Hint::new();
+    // let hint = Hint::new();
 
-    let fmt_opts: FormatOptions = Default::default();
-    let meta_opts: MetadataOptions = Default::default();
-    let dec_opts: AudioDecoderOptions = Default::default();
+    // let fmt_opts: FormatOptions = Default::default();
+    // let meta_opts: MetadataOptions = Default::default();
+    // let dec_opts: AudioDecoderOptions = Default::default();
 
-    let mut format = symphonia::default::get_probe()
-        .probe(&hint, mss, fmt_opts, meta_opts)
-        .unwrap();
+    // let mut format = symphonia::default::get_probe()
+    //     .probe(&hint, mss, fmt_opts, meta_opts)
+    //     .unwrap();
 
-    let track = format.default_track(TrackType::Audio).unwrap().clone();
+    // let track = format.default_track(TrackType::Audio).unwrap().clone();
 
-    let mut decoder = symphonia::default::get_codecs()
-        .make_audio_decoder(
-            track.codec_params.as_ref().unwrap().audio().unwrap(),
-            &dec_opts,
-        )
-        .unwrap();
-    let track_id = track.id;
-    let mut samples: Vec<f32> = Default::default();
+    // let mut decoder = symphonia::default::get_codecs()
+    //     .make_audio_decoder(
+    //         track.codec_params.as_ref().unwrap().audio().unwrap(),
+    //         &dec_opts,
+    //     )
+    //     .unwrap();
+    // let track_id = track.id;
+    // let mut samples: Vec<f32> = Default::default();
 
-    #[cfg(debug_assertions)]
-    {
-        println!("Starting audio sample parsing. This can take a fat minute in dev so be patient.");
-    }
-    while let Some(packet) = format.next_packet().unwrap() {
-        if packet.track_id != track_id {
-            continue;
-        }
+    // #[cfg(debug_assertions)]
+    // {
+    //     println!("Starting audio sample parsing. This can take a fat minute in dev so be patient.");
+    // }
+    // while let Some(packet) = format.next_packet().unwrap() {
+    //     if packet.track_id != track_id {
+    //         continue;
+    //     }
 
-        match decoder.decode(&packet) {
-            Ok(audio_buf) => {
-                let mut t: Vec<f32> = Default::default();
-                t.resize(audio_buf.samples_interleaved(), 0_f32);
-                audio_buf.copy_to_slice_interleaved(&mut t);
-                samples.append(&mut t);
-            }
-            Err(Error::DecodeError(_)) => (),
-            Err(_) => break,
-        }
-    }
-    println!("{:?}", samples.len());
+    //     match decoder.decode(&packet) {
+    //         Ok(audio_buf) => {
+    //             let mut t: Vec<f32> = Default::default();
+    //             t.resize(audio_buf.samples_interleaved(), 0_f32);
+    //             audio_buf.copy_to_slice_interleaved(&mut t);
+    //             samples.append(&mut t);
+    //         }
+    //         Err(Error::DecodeError(_)) => (),
+    //         Err(_) => break,
+    //     }
+    // }
+    // println!("{:?}", samples.len());
 
-    let binding = track.codec_params.unwrap();
-    let info = binding.audio().unwrap();
-    let sample_rate = info.sample_rate.unwrap();
-    let channels_count = info.channels.to_owned().unwrap().count();
-    println!("{} channels; sample rate: {}", channels_count, sample_rate);
-    println!("Starting Processing");
-    let five_seconds = Duration::from_secs(5);
-    let (secs, mut d) = player::create_device(
-        AudioPlayer::new(samples, sample_rate, channels_count, volume).unwrap(),
-        audio_subsystem,
-    );
-    d.resume();
-    std::thread::sleep(five_seconds);
-    d.lock().volume = 0.5_f32;
-    std::thread::sleep(five_seconds);
-    d.lock().volume = 1_f32;
-    std::thread::sleep(Duration::from_secs_f64(secs));
+    // let binding = track.codec_params.unwrap();
+    // let info = binding.audio().unwrap();
+    // let sample_rate = info.sample_rate.unwrap();
+    // let channels_count = info.channels.to_owned().unwrap().count();
+    // println!("{} channels; sample rate: {}", channels_count, sample_rate);
+    // println!("Starting Processing");
+    // let five_seconds = Duration::from_secs(5);
+    // let (secs, mut d) = player::create_device(
+    //     AudioPlayer::new(samples, sample_rate, channels_count, volume).unwrap(),
+    //     audio_subsystem,
+    // );
+    // d.resume();
+    // std::thread::sleep(five_seconds);
+    // d.lock().volume = 0.5_f32;
+    // std::thread::sleep(five_seconds);
+    // d.lock().volume = 1_f32;
+    // std::thread::sleep(Duration::from_secs_f64(secs));
     Ok(())
 }
